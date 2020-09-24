259d0d749b15c6d92365fc6f5d570d5eb69b7cbb
Fabio Peretti Guimarães <fabioperetti_pp@outlook.com>
2020-09-24 16:06:10 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+const btnscrollToEnglishMobile = document.querySelector('.enButtonMin')
+    btnscrollToEnglishMobile.addEventListener('click', function (){
+        window.scrollTo({
+            top: 1320,
+            behavior: 'smooth'
+          });
+    })
+
+    const btnscrollToPortugueseMobile = document.querySelector('.ptButtonMin')
+    btnscrollToPortugueseMobile.addEventListener('click', function (){
+        window.scrollTo({
+            top: 150,
+            behavior: 'smooth'
+          });
+    })
+
+    const btnscrollToSpanishMobile = document.querySelector('.spButtonMin')
+    btnscrollToSpanishMobile.addEventListener('click', function (){
+        window.scrollTo({
+            top: 2395,
+            behavior: 'smooth'
+          });
+    })
+
+
+    const btnscrollToEnglishScreen = document.querySelector('.enButtonMax')
+    btnscrollToEnglishScreen.addEventListener('click', function (){
+        window.scrollTo({
+            top: 600,
+            behavior: 'smooth'
+          });
+    })
+
+    const btnscrollToPortugueseScreen = document.querySelector('.ptButtonMax')
+    btnscrollToPortugueseScreen.addEventListener('click', function (){
+        window.scrollTo({
+            top: 150,
+            behavior: 'smooth'
+          });
+    })
+
+    const btnscrollToSpanishScreen = document.querySelector('.spButtonMax')
+    btnscrollToSpanishScreen.addEventListener('click', function (){
+        window.scrollTo({
+            top: 1100,
+            behavior: 'smooth'
+          });
+    })
